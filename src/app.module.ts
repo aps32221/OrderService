34ba96c2ef41831b90cd2c78716b6d9d63b8d6ef
YuEyizi <aps32777@gmail.com>
2025-03-12 00:07:49 +0800

    add routing module & usermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RouterModule } from '@nestjs/core';
+import { UserModule } from './module/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    RouterModule.register([
+      {
+        path: 'v1',
+        module: AppModule,
+        children: [
+          {
+            path: 'user',
+            module: UserModule,
+          },
+        ],
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
