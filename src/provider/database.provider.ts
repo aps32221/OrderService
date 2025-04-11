1a9eba5c68025b08c8cce20ffa98e6da0718d1ae
YuEyizi <aps32777@gmail.com>
2025-04-12 00:03:47 +0800

    add postgre database

---
+++ src/provider/database.provider.ts
@@ -0,0 +1,21 @@
+import { join } from 'path';
+import { DataSource } from 'typeorm';
+
+export const DatabaseProvider = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'Admin32221',
+        database: 'orderservice',
+        entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
+        synchronize: true,
+      });
+      return dataSource.initialize();
+    },
+  },
+];
